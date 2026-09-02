262efc67315785147e1c2dbc38f9638778132c45
Vibho <vibhos.vs@gmail.com>
2026-09-02 21:44:04 +0530

    Added frontend url in cors - main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // ADD THIS LINE: Allows our frontend to talk to the backend
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL,
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
